fedc5aebb66108eb8e9c1a944a220707c9d2557a
venominfosec <46534629+venominfosec@users.noreply.github.com>
2023-11-07 14:58:29 -0600

    Updates

--- ipinfo.py
+++ ipinfo.py
@@ -4,15 +4,16 @@
 
 # Imports
 import socket
-import requests
 import json
-import sys
+import os
+import traceback
 import argparse
+import requests
 import dns.resolver
 
 
 # Main method
-def main(input_file, output_file, verbose, api_key, very_verbose, dns):
+def main(args):
 
     # Splash screen
     print('  _       _        __        _')
@@ -26,112 +27,117 @@ def main(input_file, output_file, verbose, api_key, very_verbose, dns):
     ips = []
     resolved = []
     not_resolved = []
-    resolved_ips = ([])
+    resolved_ips = []
+    ip_to_hostname = {}
     status = 0
     line_count = 0
-    written = False
+    headers_default = ['IP', 'ASN', 'Owner', 'Country']
+    headers_dns = ['Hostname', 'IP', 'ASN', 'Owner', 'Country']
 
     # Parse IPs from input file
-    with open(input_file,'r') as data:
+    with open(args['file'], 'r') as data:
         for line in data:
-            line_count += 1
-            if isIP(line.strip()):
-                ips.append(line.strip())
-            else:
-                if dns:
-                    resolved_name = resolve_ip(line.strip())
-                    if isIP(resolved_name):
-                        ips.append(resolved_name)
-                    else:
-                        print('[!] ERROR::'+input_file+':'+str(line_count)+':'+line.strip())+' - Unable to resolve hostname'
+            line = line.strip()
+            if len(line) > 0:
+                line_count += 1
+                if is_ip(line):
+                    ip_to_hostname[line] = None
+                    ips.append(line)
                 else:
-                    print('[!] ERROR:NON-IP:'+input_file+':'+str(line_count)+':'+line.strip())
+                    if dns:
+                        resolved_ip = resolve_ip(line)
+                        if is_ip(resolved_ip):
+                            ip_to_hostname[resolved_ip] = line
+                            ips.append(resolved_ip)
+                        else:
+                            print(f'[!] ERROR:RESOLUTION:{args["file"]}:{str(line_count)}:{line}')
+                    else:
+                        print(f'[!] ERROR:NON-IP:{args["file"]}:{str(line_count)}:{line}')
 
     # Retrieve info from IPinfo and parse
     for ip in ips:
         # Local variables
-        temp_lst = []
         asn = ''
-        owner = ''
-        country = ''
 
         # Print update
         status += 1
-        if verbose:
+        if args['verbose']:
             print('['+str(status)+'/'+str(len(ips))+'] '+str(ip), end='\r')
-        elif very_verbose:
+        elif args['very_verbose']:
             print('['+str(status)+'/'+str(len(ips))+'] '+str(ip))
         else:
             print('Status: ['+str(status)+'/'+str(len(ips))+']', end='\r')
 
         # Get info and convert to JSON
-        webpage = ''
+        url = requests.get('https://ipinfo.io/'+str(ip)+'/json')
+        if args['key']:
+            url += '?token='+str(args['key'])
         try:
-            if api_key:
-                webpage = requests.get('https://ipinfo.io/'+str(ip)+'/json?token='+str(api_key))
-            else:
-                webpage = requests.get('https://ipinfo.io/'+str(ip)+'/json')
-        except requests.exceptions.ConnectionError:
-            print('[!] Connection error caught')
-        if webpage:
+            webpage = requests.get('https://ipinfo.io/'+str(ip)+'/json')
             try:
                 results = json.loads(webpage.text)
-            except json.JSONDecodeError:
-                print('[!] ERROR: recived non-JSON object for '+str(ip))
-
-            # Parse info
-            try:
-                org = str(results['org'])
-            except KeyError:
-                org = 'No org info'
-            try:
-                country = str(results['country'])
-            except KeyError:
-                country = 'No country info'
-            try:
-                if org != 'No org info':
-                    org = str(org).replace(',','')
+                # Parse info
+                try:
+                    org = remove_comma(str(results['org']))
                     if ' ' in org:
                         asn = str(org.split(' ')[0]).strip()
                         owner = str(org.split(asn)[1]).strip()
                     else:
                         owner = str(org)
-                else:
+                except KeyError:
                     asn = 'No ASN info'
                     owner = 'No owner info'
-            except KeyError:
-                owner = 'No owner info'
+                try:
+                    country = remove_comma(str(results['country']))
+                except KeyError:
+                    country = 'No country info'
 
-            # Write data to list
-            temp_lst = [ip, asn, owner, country]
-            if very_verbose:
-                print('\t  '+str(temp_lst))
-            resolved_ips.append(temp_lst)
-            resolved.append(ip)
-        else:
+                # Save results
+                temp_lst = [ip, asn, owner, country]
+                if args['very_verbose']:
+                    print('\t  ' + str(temp_lst))
+                resolved_ips.append(temp_lst)
+                resolved.append(ip)
+
+            # Error, invalid JSON
+            except json.JSONDecodeError:
+                print('[!] ERROR: received non-JSON object for '+str(ip))
+                not_resolved.append(ip)
+
+        # Error, requests error
+        except requests.exceptions.ConnectionError as error:
+            print(f'[ERROR] Connection error: {error}')
+            print(traceback.format_exc())
             not_resolved.append(ip)
 
-    # Write results to output
-    while written == False:
-        try:
-            with open(output_file,'w') as output:
-                output.write('IP,ASN,Owner,Country\n')
-                for ip,asn,owner,country in (resolved_ips):
-                    output.write(str(ip)+','+str(asn)+','+str(owner).replace('"','').replace(',','')+','+str(country).replace('"','').replace(',','')+'\n')
-            written = True
-        except PermissionError:
-            input('[!] Please close '+str(output_file)+' and hit enter: ')
+    # Write results
+    with open(args['output'], 'w') as output:
+        output.write(','.join(headers_default) if not dns else ','.join(headers_dns))
+        output.write('\n')
+        for result_list in resolved_ips:
+            if not dns:
+                output.write(','.join(result_list))
+                output.write('\n')
+            else:
+                output.write(ip_to_hostname[result_list[0]]+','+','.join(result_list))
+                output.write('\n')
 
     # Print final statuses
-    print('\nData written to '+output_file)
+    print()
+    print('Data written to '+args['output'])
     if not_resolved:
         print('\n Unable to query IPinfo.io for the following IPs:')
         for ip in not_resolved:
-            print('    '+str(ip))
+            print(f'{" "*4}*{ip}')
+
+
+# Remove commas from string
+def remove_comma(string):
+    return string.replace(',', '')
 
 
 # Verify if a given string is an IP
-def isIP(addr):
+def is_ip(addr):
     try:
         socket.inet_aton(addr)
         return True
@@ -142,54 +148,35 @@ def isIP(addr):
 # Resolve passed hostname to IP
 def resolve_ip(hostname):
     try:
-        result = dns.resolver.query(hostname)
-        dns.resolver.query(hostname)
+        result = dns.resolver.resolve(hostname)
+        dns.resolver.resolve(hostname)
         return str(result[0])
-    except dns.resolver.NXDOMAIN:
+    except dns.exception.DNSException:
         return None
-    except TypeError:
-        return None
-    except dns.exception.Timeout:
-        return None
-    except dns.resolver.NoNameservers:
-        return None
-    except dns.resolver.NoAnswer:
-        return None
-    except dns.name.LabelTooLong:
+    except Exception as error:
+        print(f'[ERROR] Uncaught exception: {error}')
+        print(traceback.format_exc())
         return None
 
 
-# Launch program
+# Run from CLI
 if __name__ == '__main__':
     # Parse arguments
-    parser = argparse.ArgumentParser(description='Retrive IP information using IPinfo.io')
+    parser = argparse.ArgumentParser(description='Retrieve IP information using IPinfo.io')
     parser.add_argument('-f', '--file', help='Argument is a file with list of CIDRs, one per line', required=True)
     parser.add_argument('-o', '--output', help='CSV output file, default = input_file-results.csv')
     parser.add_argument('-v', '--verbose', help='Verbose mode', action='store_true')
     parser.add_argument('-vv', '--very-verbose', help='Print results as they are retrieved', action='store_true')
     parser.add_argument('-k', '--key', help='IPinfo key to use')
     parser.add_argument('--dns', help='Resolve input lines that aren\'t IP addresses', action='store_true')
-    args = parser.parse_args()
-    arg_file = args.file
-    arg_output = args.output
-    arg_verbose = args.verbose
-    arg_very_verbose = args.very_verbose
-    arg_key = args.key
-    arg_dns = args.dns
+    raw_args = parser.parse_args()
+    args = raw_args.__dict__
 
-    # Argument validation
-    if arg_file:
-        try:
-            with open(arg_file,'r') as test_output:
-                None
-        except IOError:
-            print('[!] ERROR: Provided input file "'+str(arg_file)+'" cannot be opened')
-            sys.exit()
-    if arg_output:
-        if '.' not in arg_output:
-            arg_output += '.csv'
-    else:
-        arg_output = arg_file+'-results.csv'
+    # Validate args
+    if args['file'] and not os.path.isfile(args['file']):
+        parser.error(f'File {args["file"]} does not exist')
+    if not args['output']:
+        args['output'] = args['file']+'-results.csv'
 
-    # launch program
-    main(arg_file, arg_output, arg_verbose, arg_key, arg_very_verbose, arg_dns)
+    # Run
+    main(args)
